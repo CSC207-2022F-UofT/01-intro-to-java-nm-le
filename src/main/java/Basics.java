c3cdcd2113e5582561e1922101b260a3be0fe23e
Minh Le <ngocminhle318@gmail.com>
2022-10-08 23:35:49 -0400

    Submit

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -2,7 +2,7 @@
  * This file contains a few exercises to familiarize you with Java.
  * You should read this file from top-to-bottom. Any tasks you are to complete
  * are labelled with TODO
- *
+
  * For your convenience, we have also included references to the
  * relevant readings for each task.
  */
@@ -10,9 +10,9 @@
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
- *
+
  *    For this exercise, all of our code is within the Basics class.
- *
+
  *    (Relevant reading: 1.1.1. Defining classes)
  */
 public class Basics {
@@ -43,8 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
-
+        System.out.println("Hello World!");
 
         /* 4. In Python, we could declare variables using a simple assignment
          *    statement. For example:
@@ -62,6 +61,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,8 +99,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
-
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
     }
 
     /**
@@ -141,9 +142,10 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
-
-        // Fill in the rest of the body here
-
+        String[] words = to_split.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            ret.append(words[i].charAt(0));
+        }
         return ret.toString();
     }
 
@@ -163,6 +165,15 @@ public class Basics {
      */
     public static int oddSum(int[] arr) {
         int current_sum = 0;
+        if (arr.length == 0 || arr.length == 1) {
+            return 0;
+        }
+        else {
+            for (int i = 1; i < arr.length; i = i + 2) {
+                current_sum += arr[i];
+            }
+        }
+        return current_sum;
 
         /* TODO (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
@@ -171,7 +182,6 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
-        return current_sum;
     }
 
 
